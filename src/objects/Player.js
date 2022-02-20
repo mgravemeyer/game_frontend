2c795cac16e7a5a5a644b9782ff2f374ac317893
mgravemeyer <m.gravemeyer@icloud.com>
2022-02-20 01:02:31 +0100

    added idle/run states

--- src/objects/Player.js
+++ src/objects/Player.js
@@ -12,10 +12,15 @@ class Player extends Phaser.Physics.Matter.Sprite {
         scene.load.animation('archer_anim', 'assets/images/archer_anim.json');
     }
 
+    get velocity() {
+        return this.body.velocity;
+    }
+
     update() {
-        this.anims.play('archer_walk', true);
+
         const speed = 2.5;
         let playerVelocity = new Phaser.Math.Vector2();
+
         if (this.inputKeys.left.isDown) {
             playerVelocity.x = -1;
         } else if (this.inputKeys.right.isDown) {
@@ -26,9 +31,15 @@ class Player extends Phaser.Physics.Matter.Sprite {
         } else if (this.inputKeys.down.isDown) {
             playerVelocity.y = 1;
         }
+
         playerVelocity.normalize();
         playerVelocity.scale(speed);
         this.setVelocity(playerVelocity.x, playerVelocity.y);
+        if(Math.abs(this.velocity.x) > 0.1 || Math.abs(this.velocity.y) > 0.1) {
+            this.anims.play('archer_walk', true);
+        } else {
+            this.anims.play('archer_idle', true);
+        }
     }
 }
 
