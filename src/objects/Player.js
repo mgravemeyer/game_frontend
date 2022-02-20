0e24c113c46b9e595db1ac481991dce24fef85d5
mgravemeyer <m.gravemeyer@icloud.com>
2022-02-20 01:09:51 +0100

    added new compoundBody for player

--- src/objects/Player.js
+++ src/objects/Player.js
@@ -5,6 +5,16 @@ class Player extends Phaser.Physics.Matter.Sprite {
         let {scene, x, y, texture, frame} = data;
         super(scene.matter.world, x, y, texture, frame);
         this.scene.add.existing(this);
+
+        const { Body, Bodies } = Phaser.Physics.Matter.Matter;
+        var playerCollieder = Bodies.circle(this.x, this.y, 12, {isSensor: false, label:'playerCollider'});
+        var playerSensor = Bodies.circle(this.x, this.y, 24, {isSensor: true, label:'playerSensor'});
+        const compoundBody = Body.create({
+            parts:[playerCollieder, playerSensor],
+            frictionAir: 0.35,
+        });
+        this.setExistingBody(compoundBody);
+        this.setFixedRotation();
     }
 
     static preload(scene) {
